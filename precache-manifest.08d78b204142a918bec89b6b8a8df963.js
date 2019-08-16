6a3cc135e0a33666b5ac6c962b9e86d7bb9fbbaf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-16 14:09:07 +0000

    Deploy ninja-labs-tech/NinjaLabs.tech to github.com/ninja-labs-tech/NinjaLabs.tech.git:gh-pages

---
+++ precache-manifest.08d78b204142a918bec89b6b8a8df963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48a6c051a40da4a0ffb4842b8da246a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf370de83d0b38c082a",
+    "url": "/static/css/main.0e698053.chunk.css"
+  },
+  {
+    "revision": "d75856be254732d41dd9",
+    "url": "/static/js/2.c9042b03.chunk.js"
+  },
+  {
+    "revision": "bcf370de83d0b38c082a",
+    "url": "/static/js/main.2e6165e3.chunk.js"
+  },
+  {
+    "revision": "782771e0d8cfa729948a",
+    "url": "/static/js/runtime~main.d494b1c9.js"
+  }
+]);
